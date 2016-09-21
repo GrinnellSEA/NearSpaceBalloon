9a65a3ad433a527399a473e60f3285a16e9e6832
Cory McCartan <cory.mccartan@gmail.com>
2016-09-21 17:06:30 -0500

    working testing and logging code

--- comp/src/main.ino
+++ comp/src/main.ino
@@ -1,6 +1,6 @@
 /*
  * NEAR SPACE BALLOON PROJECT
- * FLIGHT COMPUTER CODE
+ * TEST AND LOGGING CODE
  *
  * GRINNELL SPACE EXPLORATION AGENCY
  *
@@ -11,7 +11,8 @@
 #include "temp.h"
 #include "pressure.h"
 
-#define FIVE_MINUTES (5*60*1000)
+#define INTERVAL (1*10*1000)
+#define BLINK (500)
 
 int address = 0;
 int max_addr;
@@ -25,22 +26,25 @@ void setup() {
     for (int a = 0; a < max_addr; a++)
         EEPROM.update(a, 0);
 
-//    setupTemperatureSensor() || error("Temperature sensor setup failed.");
-//    setupPressureSensor() || error("Pressure sensor setup failed.");
+    // setup LED_BUILTIN blink
+    pinMode(LED_BUILTIN, OUTPUT);
+    digitalWrite(LED_BUILTIN, LOW);
+
+    setupTemperatureSensor() || error("Temperature sensor setup failed.");
+    setupPressureSensor() || error("Pressure sensor setup failed.");
 }
 
 // runs continuously
 void loop() {
-//    double kelvin = getTemperature();
-//    double farenheit = toFarenheit(kelvin);
-//    byte temp_int = (int) map(farenheit, 0, 51, 0, 255);
-    byte temp_int = 5;
-    byte pres_int = 7;
-//    double mb = getPressure(); // don't do anything with it.
-//    byte pres_int = (int) map(mb, 950, 1050, 0, 255);
+    double kelvin = getTemperature();
+    double farenheit = toFarenheit(kelvin);
+    byte temp_int = memCompress(farenheit, 0, 102);
+
+    double mb = getPressure(); // don't do anything with it.
+    byte pres_int = memCompress(mb, 950, 1050);
 
     long voltage = readVcc();
-    byte vcc_int = (int) map(voltage, 2000, 7100, 0, 255);
+    byte vcc_int = memCompress(voltage, 2000, 7100);
 
     // store data
     EEPROM.update(address + 0, temp_int);
@@ -50,7 +54,10 @@ void loop() {
     if (address + 3 >= max_addr) // If we'd run out of space next time
        address = 0; 
 
-    delay(FIVE_MINUTES); 
+    digitalWrite(LED_BUILTIN, HIGH);
+    delay(BLINK); 
+    digitalWrite(LED_BUILTIN, LOW);
+    delay(INTERVAL - BLINK); 
 }
 
 void end() { while (true) delay(1000); }
@@ -66,15 +73,7 @@ bool error(char *msg) {
 long readVcc() {
     // Read 1.1V reference against AVcc
     // set the reference to Vcc and the measurement to the internal 1.1V reference
-#if defined(__AVR_ATmega32U4__) || defined(__AVR_ATmega1280__) || defined(__AVR_ATmega2560__)
-    ADMUX = _BV(REFS0) | _BV(MUX4) | _BV(MUX3) | _BV(MUX2) | _BV(MUX1);
-#elif defined (__AVR_ATtiny24__) || defined(__AVR_ATtiny44__) || defined(__AVR_ATtiny84__)
-    ADMUX = _BV(MUX5) | _BV(MUX0);
-#elif defined (__AVR_ATtiny25__) || defined(__AVR_ATtiny45__) || defined(__AVR_ATtiny85__)
-    ADMUX = _BV(MUX3) | _BV(MUX2);
-#else
     ADMUX = _BV(REFS0) | _BV(MUX3) | _BV(MUX2) | _BV(MUX1);
-#endif  
 
     delay(2); // Wait for Vref to settle
     ADCSRA |= _BV(ADSC); // Start conversion
@@ -87,4 +86,8 @@ long readVcc() {
 
     result = 1125300L / result; // Calculate Vcc (in mV); 1125300 = 1.1*1023*1000
     return result; // Vcc in millivolts
+}
+
+byte memCompress(double value, double in_min, double in_max) {
+    return (byte) ((value - in_min) * 255 / (in_max - in_min));
 }
--- read_data/src/main.ino
+++ read_data/src/main.ino
@@ -26,21 +26,25 @@ void loop() {
     if (temp_int == 0 && pres_int == 0 && vcc_int == 0) // end of data
         end();
 
-    double temperature = map(temp_int, 0, 255, 0, 51);
-    double pressure = map(pres_int, 0, 255, 950, 1050);
-    double voltage = map(vcc_int, 0, 255, 2000, 7100);
+    double temperature = memDecompress(temp_int, 0, 102);
+    double pressure = memDecompress(pres_int, 950, 1050);
+    double voltage = memDecompress(vcc_int, 2.0, 7.1);
     int minutes = address / 3 * 5;
    
     Serial.print(minutes);
-    Serial.print(" minutes: \t");
+    Serial.print(" seconds: \t");
     Serial.print(temperature);
-    Serial.print(" °F\t");
+    Serial.print(" °F \t");
     Serial.print(pressure);
-    Serial.print(" mb\t");
+    Serial.print(" mb \t");
     Serial.print(voltage);
-    Serial.print(" V\n");
+    Serial.print(" V\r\n");
 
     address += 3;
 }
 
 void end() { while(true) delay(10e3); }
+
+double memDecompress(int value, double out_min, double out_max) {
+    return ((double) value) * (out_max - out_min) / 255 + out_min;
+}
